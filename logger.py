6b87d35110adb78fad036c618b6d3c9d2e998a31
Quin <quin.marilyn05@gmail.com>
2023-02-18 11:23:46 -0700

    Add a debug logger.

--- logger.py
+++ logger.py
@@ -5,3 +5,4 @@ def setup():
 	log_path = Path.cwd() / "logs"
 	log_path.mkdir(parents=True, exist_ok=True)
 	logger.add(log_path / "error.log", level="ERROR", retention="10 days")
+	logger.add(log_path / "debug.log", level="DEBUG", retention="1 day")
